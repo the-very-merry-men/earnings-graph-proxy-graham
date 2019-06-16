748943c9a5805d1d3b2942209d6ec9eb6515e344
Grahams Laptop <doughertyg37@gmail.com>
2019-06-16 15:50:52 -0700

    modified server file and html to serve up all components

--- server/server.js
+++ server/server.js
@@ -10,33 +10,59 @@ const stocks = require('./stocks');
 
 const app = express();
 
-const PORT = 3000;
+const PORT = 3003;
 
 app.use(express.static(path.join(__dirname,'../client/public')));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/api/stocks/:stock/earnings', (req, res) => {
-  const newUrl = 'http://52.14.233.205:3000';
+  // const newUrl = 'http://52.14.233.205:3000';
+  const newUrl = 'http://localhost:3000';
   request(`${newUrl}/api/stocks/${req.params.stock}/earnings`).pipe(res);
 });
 
+app.post('/api/stocks/:stock/earnings', (req, res) => { //posting new earnings
+
+  const newUrl = 'http://localhost:3000';
+  request(`${newUrl}/api/stocks/${req.params.stock}/earnings`, (err, data) => {
+    if (err) {
+      console.log('theres been an error', err);
+      res.status(500);
+      res.send(err);
+    }
+    
+    res.status(200);
+    res.send(data);
+  });
+})
+
 app.get('/api/stocks/:stock/price', (req, res) => {
-  const newUrl = 'http://3.17.135.66:3000';
-  request(`${newUrl}/api/stocks/${req.params.stock}/price`).pipe(res);
+  // const newUrl = 'http://3.17.135.66:3000';
+  const newUrl = 'http://localhost:3002';
+  request(`${newUrl}/api/stocks/${req.params.stock}/price`)
+  .on('error', (err) => {console.log('error talking to localhost db!', err)})
+  .pipe(res);
 });
 
 app.get('/api/stocks/:stock/paid', (req, res) => {
-  const newUrl = 'http://localhost:3003';
-  request(`${newUrl}/api/stocks/${req.params.stock}/paid`).pipe(res);
+  const newUrl = 'http://localhost:3111';
+  request(`${newUrl}/api/stocks/${req.params.stock}/`).pipe(res);
 });
 
 app.get('/api/stocks/:stock/prices/:time', (req, res) => {
-  const newUrl = 'http://localhost:3004';
+  const newUrl = 'http://localhost:3001';
   request(`${newUrl}/api/stocks/${req.params.stock}/prices/${req.params.time}`).pipe(res);
 });
 
 app.get('/stocks/:stock', (req, res) => {
   res.sendFile(path.join(__dirname,'../client/public/index.html'));
 });
+
+// port 3111  =>  prices paid
+// port 3001  =>  stock chart
+// port 3003  =>  earnings
+// port 3002  =>  trade panel
+//
+
 
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
